9fe0af562824604923fc2c909e8d069e69df61b0
SmartBlueSukukToken <88102202+SmartBlueSukukToken@users.noreply.github.com>
2021-07-29 22:48:51 +0200

    Update Address.sol

--- Address.sol
+++ Address.sol
@@ -1,13 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-/**
- * Collection of functions related to the address type
- */
+//Collection of functions related to the address type
 library Address {
-    /**
-     *Returns true if `account` is a contract.
-     */
+    //Returns true if `account` is a contract.
     function isContract(address account) internal view returns (bool) {
         // This method relies in extcodesize, which returns 0 for contracts in
         // construction, since the code is only stored at the end of the
@@ -19,10 +15,9 @@ library Address {
         return size > 0;
     }
 
-    /**
-     * Replacement for Solidity's `transfer`: sends `amount` wei to
-     * `recipient`, forwarding all available gas and reverting on errors.
-     */
+    // Replacement for Solidity's `transfer`: sends `amount` wei to
+    //`recipient`, forwarding all available gas and reverting on errors.
+    
     function sendValue(address payable recipient, uint256 amount) internal {
         require(address(this).balance >= amount, "Address: insufficient balance");
 
@@ -31,40 +26,31 @@ library Address {
         require(success, "Address: unable to send value, recipient may have reverted");
     }
 
-    /**
-     * Performs a Solidity function call using a low level `call`. A
-     * plain`call` is an unsafe replacement for a function call: use this
-     * function instead.
-     */
+    //Performs a Solidity function call using a low level `call`. A
+    //plain`call` is an unsafe replacement for a function call: use this
+    //function instead.
+
     function functionCall(address target, bytes memory data) internal returns (bytes memory) {
       return functionCall(target, data, "Address: low-level call failed");
     }
 
-    /**
-     * Same as {xref-Address-functionCall-address-bytes-}[`functionCall`], but with
-     * `errorMessage` as a fallback revert reason when `target` reverts.
-     */
+    //Same as {xref-Address-functionCall-address-bytes-}[`functionCall`], but with
+    //`errorMessage` as a fallback revert reason when `target` reverts.
+  
     function functionCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
         return _functionCallWithValue(target, data, 0, errorMessage);
     }
 
-    /**
-     * Same as {xref-Address-functionCall-address-bytes-}[`functionCall`],
-     * but also transferring `value` wei to `target`.
-     *
-     * Requirements:
-     *
-     * - the calling contract must have an ETH balance of at least `value`.
-     * - the called Solidity function must be `payable`.
-     */
+    // Same as {xref-Address-functionCall-address-bytes-}[`functionCall`],
+    // but also transferring `value` wei to `target`.
+     
     function functionCallWithValue(address target, bytes memory data, uint256 value) internal returns (bytes memory) {
         return functionCallWithValue(target, data, value, "Address: low-level call with value failed");
     }
 
-    /**
-     * @dev Same as {xref-Address-functionCallWithValue-address-bytes-uint256-}[`functionCallWithValue`], but
-     * with `errorMessage` as a fallback revert reason when `target` reverts.
-     */
+    // Same as {xref-Address-functionCallWithValue-address-bytes-uint256-}[`functionCallWithValue`], but
+    // with `errorMessage` as a fallback revert reason when `target` reverts.
+  
     function functionCallWithValue(address target, bytes memory data, uint256 value, string memory errorMessage) internal returns (bytes memory) {
         require(address(this).balance >= value, "Address: insufficient balance for call");
         return _functionCallWithValue(target, data, value, errorMessage);
@@ -93,4 +79,3 @@ library Address {
         }
     }
 }
-
